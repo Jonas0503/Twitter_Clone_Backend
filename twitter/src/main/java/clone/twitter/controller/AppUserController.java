57e82afdcbda3c10a8ab97fbd9f1f276c6bf744d
Jonas Gilbert <jonas.gilbert.0503@gmail.com>
2025-06-09 16:19:15 +0200

    Added REST controllers for managing users and posts

--- twitter/src/main/java/clone/twitter/controller/AppUserController.java
+++ twitter/src/main/java/clone/twitter/controller/AppUserController.java
@@ -1,7 +1,48 @@
 package clone.twitter.controller;
 
-import org.springframework.stereotype.Controller;
+import clone.twitter.dto.AppUserDTO;
+import clone.twitter.service.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.net.URI;
+import java.util.UUID;
+
+// TODO: Exception Handling
+@RestController
 public class AppUserController {
+
+    private AppUserService appUserService;
+
+    @Autowired
+    public AppUserController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<AppUserDTO> getAppUser(@PathVariable UUID id) {
+        AppUserDTO appUserDTO = appUserService.readAppUser(id);
+        return ResponseEntity.ok(appUserDTO);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<AppUserDTO> createNewUser(@RequestBody AppUserDTO appUserDTO) {
+        AppUserDTO createdAppUserDTO = appUserService.createAppUser(appUserDTO);
+        UUID id = createdAppUserDTO.getId();
+        return ResponseEntity.created(URI.create("/user/{id}")).body(createdAppUserDTO);
+    }
+
+    @PutMapping("/user")
+    public ResponseEntity<AppUserDTO> updateUser(@RequestBody AppUserDTO appUserDTO) {
+        AppUserDTO updatedUser = appUserService.updateAppUser(appUserDTO);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable UUID id) {
+        if (appUserService.deleteAppUser(id)) return ResponseEntity.noContent().build();
+        else return ResponseEntity.badRequest().build();
+    }
+
 }
--- twitter/src/main/java/clone/twitter/controller/PostController.java
+++ twitter/src/main/java/clone/twitter/controller/PostController.java
@@ -1,7 +1,47 @@
 package clone.twitter.controller;
 
-import org.springframework.stereotype.Controller;
+import clone.twitter.dto.PostDTO;
+import clone.twitter.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.net.URI;
+import java.util.UUID;
+
+// TODO: Exception Handling
+@RestController
 public class PostController {
+
+    private PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/post/{id}")
+    public ResponseEntity<PostDTO> getPost(@PathVariable UUID id) {
+        PostDTO postDTO = postService.readPost(id);
+        return ResponseEntity.ok(postDTO);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<PostDTO> createNewPost(@RequestBody PostDTO postDTO) {
+        PostDTO createdPostDTO = postService.createPost(postDTO);
+        UUID id = createdPostDTO.getId();
+        return ResponseEntity.created(URI.create("/post/{id}")).body(createdPostDTO);
+    }
+
+    @PutMapping("/post")
+    public ResponseEntity<PostDTO> updatePost(@RequestBody PostDTO postDTO) {
+        PostDTO updatedPost = postService.updatePost(postDTO);
+        return ResponseEntity.ok(updatedPost);
+    }
+
+    @DeleteMapping("/post/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable UUID id) {
+        if (postService.deletePost(id)) return ResponseEntity.noContent().build();
+        else return ResponseEntity.badRequest().build();
+    }
 }
